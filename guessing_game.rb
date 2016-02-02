59ab5730e5d375ff168e6e703766314986419e29
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-02 14:34:52 -0500

    Fix repeat guesses behavior

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,43 +1,41 @@
-def is_too_high?(user_input,random_number)
-  if user_input > random_number
-    return true
+def check_guess(u, c)
+  if u > c
+    return "high"
+  elsif u < c
+    return "low"
   else
-    return false
+    return "true"
   end
 end
 
-def is_too_low?(user_input,random_number)
-  if user_input < random_number
-    return true
-  else
-    return false
+def repeat_guess?(n, guesses)
+  (guesses.length - 1).times do |i|
+    if guesses[i] == guesses[n]
+      return true
+      break
+    end
   end
+  return false
 end
 
-random_number = rand(1..100)
+computer = rand(1..100)
+guesses = []
 
 5.times do |n|
 
-  puts "Try and guess my number between 1 and 100.. you have #{5-n} guess(es) left"
-  user_input = gets.chomp.to_i
-  last_guess = user_input
+  puts "Try and guess my number between 1 and 100"
+  guesses[n] = gets.chomp.to_i
 
-  if !is_too_low?(user_input, random_number) && !is_too_high?(user_input, random_number)
-    puts "Correctamundo, #{user_input} is the right number!"
-    break
-  else
-    if last_guess == user_input && n > 0
-        puts "Really - same guess as last time?"
-    end
-    if n == 4
-      puts "Sorry, you lose. The correct number was #{random_number}"
-    elsif is_too_low?(user_input, random_number)
-      puts "Too low"
-    else
-      puts "Too high"
-    end
+  if repeat_guess?(n, guesses)
+    puts "Really, repeating guesses? Okay.."
   end
-
-  last_guess = user_input
+  if check_guess(guesses[n], computer) == "true"
+    puts "Correctamundo, #{guesses[n]} is the right number!"
+    break
+  elsif n == 4
+      puts "Sorry, you lose. The correct number was #{computer}"
+  else
+      puts "Too #{check_guess(guesses[n], computer)}"
+  end
 
 end
