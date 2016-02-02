91e859e8856df1218474168ae26baf74f0fd71f3
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-02 13:21:40 -0500

    Refactor check_guess method to is_too_low? and is_too_high? & quit game after 5 guesses

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,23 +1,36 @@
 random_number = rand(1..100)
 
-def check_guess(user_input,random_number)
+def is_too_high?(user_input,random_number)
   if user_input > random_number
-    puts "Too high, try again"
-    return false
-  elsif user_input < random_number
-    puts "Too low, try again"
-    return false
-  else
-    puts "Correctamundo! Nice work"
     return true
+  else
+    return false
+  end
+end
+
+def is_too_low?(user_input,random_number)
+  if user_input < random_number
+    return true
+  else
+    return false
   end
 end
 
 5.times do |n|
-  puts "Try and guess my number.. you have #{5-n} guesses left"
+
+
+  puts "Try and guess my number between 1 and 100.. you have #{5-n} guesses left"
   user_input = gets.chomp.to_i
 
-  if check_guess(user_input, random_number)
-    break
+  if !is_too_low?(user_input, random_number) && !is_too_high?(user_input, random_number)
+    puts "Correctamundo, #{user_input} is the right number!"
+  elsif (is_too_low?(user_input, random_number) || is_too_high?(user_input, random_number)) && (n == 4)
+    puts "Sorry, you lose"
+  else
+    if is_too_low?(user_input, random_number)
+      puts "Too low"
+    else
+      puts "Too high"
+    end
   end
 end
