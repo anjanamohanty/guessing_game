21da28de316952dcf72df695387485f1e893b581
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-02 23:30:29 -0500

    Update binary search to work with user input

--- computer_guessing.rb
+++ computer_guessing.rb
@@ -1,13 +1,15 @@
-def bsearch(range, x)
+def bsearch(range)
   mid_index = range.length / 2
-  if range[mid_index] < x
-    bsearch(range[(mid_index + 1)..(range.length - 1)], x)
-  elsif range[mid_index] > x
-    bsearch(range[0..(mid_index - 1)], x)
+  puts "Is #{range[mid_index]} your number? Respond 'lower', 'higher', or 'yes'"
+  user_input = gets.chomp
+  if user_input == "higher"
+    bsearch(range[(mid_index + 1)..(range.length - 1)])
+  elsif user_input == "lower"
+    bsearch(range[0..(mid_index - 1)])
   else
     return range[mid_index]
   end
 end
 
-# puts "Think of a number between 1 and 100"
-puts bsearch((1..100).to_a, 50)
+puts "Think of a number between 1 and 100"
+puts "#{bsearch((1..100).to_a)} is your number!"
