6d169a7b0eb5f3b8d59a98f27d0bad574e51343f
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-03 00:25:14 -0500

    Add lie detection

--- computer_guessing.rb
+++ computer_guessing.rb
@@ -2,14 +2,27 @@ def bsearch(range)
   mid_index = range.length / 2
   puts "Is #{range[mid_index]} your number? Respond 'lower', 'higher', or 'yes'"
   user_input = gets.chomp
-  if user_input == "higher"
-    bsearch(range[(mid_index + 1)..(range.length - 1)])
-  elsif user_input == "lower"
-    bsearch(range[0..(mid_index - 1)])
+  if user_input == "yes"
+    return "#{range[mid_index]} is your number!"
   else
-    return range[mid_index]
+    if mid_index == 0
+      puts "Lies and deceit!"
+    elsif user_input == "lower"
+      if range[mid_index] == range.first
+        puts "Lies and deceit!"
+      else
+        bsearch(range[0..(mid_index - 1)])
+      end
+    else
+      if range[mid_index] == range.last
+        puts "Lies and deceit"
+      else
+        bsearch(range[(mid_index + 1)..(range.length - 1)])
+      end
+    end
   end
 end
 
+
 puts "Think of a number between 1 and 100"
-puts "#{bsearch((1..100).to_a)} is your number!"
+puts bsearch((1..100).to_a)
